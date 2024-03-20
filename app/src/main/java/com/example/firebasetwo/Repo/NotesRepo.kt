4d19d68b500bdde9e00e3b1ff7c98c9519f4594e
Shruti <shrutiarora2507@gmail.com>
2024-03-20 18:05:15 +0530

    Comit

--- app/src/main/java/com/example/firebasetwo/Repo/NotesRepo.kt
+++ app/src/main/java/com/example/firebasetwo/Repo/NotesRepo.kt
@@ -4,12 +4,23 @@ import androidx.lifecycle.LiveData
 import com.example.firebasetwo.dao.Dao
 import com.example.firebasetwo.model.Notes
 
-class NotesRepo(val dataAccessObject:Dao) {
+class NotesRepo(val dataAccessObject: Dao) {
 
-    fun getAllNotes():LiveData<List<Notes>>{
+    fun getAllNotes(): LiveData<List<Notes>> {
         return dataAccessObject.getNotes()
     }
 
+    fun insertNotes(notes: Notes) {
+        dataAccessObject.insertNotes(notes)
+    }
+
+    fun deleteNotes(id: Int) {
+        dataAccessObject.deleteNote(id)
+    }
+
+    fun updateNote(notes: Notes) {
+        dataAccessObject.updateNotes(notes)
+    }
 
 
 }
---
+++ app/src/main/java/com/example/firebasetwo/ViewModel/notesViewM.kt
@@ -0,0 +1,4 @@
+package com.example.firebasetwo.ViewModel
+
+class notesViewM {
+}
