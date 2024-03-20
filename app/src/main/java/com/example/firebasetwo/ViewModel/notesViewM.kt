1251703c634b1ca0c730f4ef838e1a82c232dcc5
Shruti <shrutiarora2507@gmail.com>
2024-03-20 19:46:57 +0530

    Comit

--- app/src/main/java/com/example/firebasetwo/ViewModel/notesViewM.kt
+++ app/src/main/java/com/example/firebasetwo/ViewModel/notesViewM.kt
@@ -1,4 +1,35 @@
 package com.example.firebasetwo.ViewModel
 
-class notesViewM {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.firebasetwo.Repo.NotesRepo
+import com.example.firebasetwo.dao.Dao
+import com.example.firebasetwo.database.database
+import com.example.firebasetwo.model.Notes
+
+class notesViewM(application: Application) : AndroidViewModel(application) {
+
+    val repository: NotesRepo
+
+    init {
+        val dao = database.getDbInstance(application).MyNotesDao()
+        repository = NotesRepo(dao)
+    }
+
+    fun addNotes(notes: Notes) {
+        repository.insertNotes(notes)
+    }
+
+    fun getNotes(): LiveData<List<Notes>> = repository.getAllNotes()
+
+    fun deleteNotes(id: Int) {
+
+        repository.deleteNotes(id)
+    }
+
+    fun updateNotes(notes: Notes){
+        repository.updateNote(notes)
+    }
+
 }
--- app/src/main/java/com/example/firebasetwo/notes/create_notes.kt
+++ app/src/main/java/com/example/firebasetwo/notes/create_notes.kt
@@ -5,20 +5,69 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.firebasetwo.R
 import com.example.firebasetwo.databinding.FragmentCreateNotesBinding
+import java.util.Date
 
 
 class create_notes : Fragment() {
 
-    private lateinit var bind:FragmentCreateNotesBinding
+    var priority = 1
+
+    private lateinit var bind: FragmentCreateNotesBinding
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        bind= FragmentCreateNotesBinding.inflate(layoutInflater,container,false)
+        bind = FragmentCreateNotesBinding.inflate(layoutInflater, container, false)
+
+
+        bind.saveNote.setOnClickListener {
+            createNote(it)
+
+
+        }
+
+
+
+        bind.greendot.setOnClickListener {
+            priority = 1
+            bind.greendot.setImageResource(R.drawable.baseline_done_24)
+            bind.reddot.setImageResource(0)
+            bind.yellowdot.setImageResource(0)
+        }
+
+        bind.reddot.setOnClickListener {
+            priority = 2
+            bind.reddot.setImageResource(R.drawable.baseline_done_24)
+            bind.greendot.setImageResource(0)
+            bind.yellowdot.setImageResource(0)
+        }
+
+        bind.yellowdot.setOnClickListener {
+            priority = 3
+            bind.yellowdot.setImageResource(R.drawable.baseline_done_24)
+            bind.reddot.setImageResource(0)
+            bind.greendot.setImageResource(0)
+        }
+
+
+
+
         return bind.root
+    }
+
+    private fun createNote(it: View) {
+        val title = bind.addTitle.text
+        val subtitle = bind.AddSubtitle.text
+        val note = bind.AddNote.text
+
+        val d = Date()
+
+        var date = android.text.format.DateFormat.format("MMMM d, YYYY ", d.time)
+
     }
 
 }
